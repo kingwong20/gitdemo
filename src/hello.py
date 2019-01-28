a386e859fa9728c4780e1c032237c80aba3f31b5
kingwong20 <k222wong@edu.uwaterloo.ca>
2019-01-28 15:39:32 -0500

    changed stuff

--- src/hello.py
+++ src/hello.py
@@ -9,15 +9,5 @@ def say_hi():
 
 if __name__== "__main__":
     say_hi()
-#prints hello
-
-import utils
-
-@utils.benchmark
-def say_hi():
-    print("hello git!")
-
-if __name__== "__main__":
-    say_hi()
 
 
